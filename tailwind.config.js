a9fdec3e59a8eff57e7c3d22d61defb8f5ca05a4
Roy Cuadra <roycuadra16@gmail.com>
2023-04-26 13:01:11 +0800

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
     extend: {
       colors: {
         "deep-blue": "#010026",
-        blue: "#2CBCE9",
-        red: "#DC4492",
-        yellow: "#FDCC49",
+        blue: "#00ccff",
+        red: "#DC143C",
+        yellow: "#BF40BF",
         grey: "#ededed",
         "dark-grey": "#757575",
         "opaque-black": "rgba(0,0,0,0.35)",
